12887aaa1346255d1a618cc8b2325b27d7cdc8e2
teachmesomething2580 <teachmesomething2580@gmail.com>
2018-10-02 00:00:24 -0700

    index, detail, results, vote view를 url과 연결, index뷰는 가장 최근에 발행된 Question을 5개 까지 보여줌

--- app/config/views.py
+++ app/config/views.py
@@ -1,19 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect
 
-
 def index(request):
     # 곧바로 /polls로 redirect 되도록 설정
     return redirect('index')
-
-
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
-
-def results(request, question_id):
-    return HttpResponse("You're looking at the results of question %s." % question_id)
-
-
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
