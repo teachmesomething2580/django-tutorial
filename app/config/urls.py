84c0753f1b9d4b0526e45b4b1288136015cc477f
teachmesomething2580 <teachmesomething2580@gmail.com>
2018-10-01 20:46:45 -0700

    path('')로 접근 시 곧바로 'index' URL(polls.views.index에 해당)로 이동하도록 config.views.index 구현

--- app/config/urls.py
+++ app/config/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from polls import views
+
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
+    path('', views.index),
 ]
---
+++ app/config/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import redirect
+
+
+def index(request):
+    # 곧바로 /polls로 redirect 되도록 설정
+    return redirect('index')
