599a73c07b1c54bca8b1678ef960b98d750907cd
teachmesomething2580 <teachmesomething2580@gmail.com>
2018-10-01 22:42:51 -0700

    polls application을 INSTALLED APP에 추가 및 Question, Choice모델 클래스 migration 생성

--- app/config/settings.py
+++ app/config/settings.py
@@ -25,6 +25,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -97,9 +99,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
